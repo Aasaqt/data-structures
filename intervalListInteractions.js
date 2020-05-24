ba3879424aeeb2455180bd184f3a4b0dd4a93ac3
Aasaqt <aasaqt01@gmail.com>
2020-05-24 12:19:18 +0530

    Leet code 23May

---
+++ intervalListInteractions.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} A
+ * @param {number[][]} B
+ * @return {number[][]}
+ */
+var intervalIntersection = function(A, B) {
+    let ans = []
+    let i = 0, j = 0;
+
+    while (i < A.length && j < B.length) {
+      let lo = Math.max(A[i][0], B[j][0]);
+      let hi = Math.min(A[i][1], B[j][1]);
+      if (lo <= hi)
+        ans.push([lo, hi]);
+
+      if (A[i][1] < B[j][1])
+        i++;
+      else
+        j++;
+    }
+
+    return ans;
+};
